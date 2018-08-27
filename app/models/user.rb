675967ca51a2b080a12e6d7e52cb35ad6d202fe3
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-08-27 11:58:54 -0400

    added password and many other changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase}
     has_one_attached :photo
     has_many :articles
+    has_secure_password
+    validates :password, presence: true
+    validates :email, presence: true, length: { maximum:100 }
+    
     def full_name
         self.first_name + " " + self.last_name
     end
+    
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        Bcrypt::Password.create(string, cost:cost)
+    end
+
 end
+
